1e26e5932a8a7d5ceb8f175679f485255a39626f
Carolanne Aleman <carolanne.aleman@gmail.com>
2020-04-22 14:40:55 -0400

    Fixed up spelling errors

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -23,21 +23,21 @@ try {
   // --------------------------------------------------
   // It should ignore additional numbers.
   var result = difference(1, 1, 1, 1, 1, 1);
-  if (result !==0) throw new Error('Expected sum(1, 1, 1, 1, 1, 1,) to be 0. Received :' + result);
+  if (result !==0) throw new Error('Expected difference(1, 1, 1, 1, 1, 1,) to be 0. Received :' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
   var result = difference(1);
-  if (result !==1) throw new Error('Expected sum(1) to be 1. Received :' + result);
+  if (result !==1) throw new Error('Expected difference(1) to be 1. Received :' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
   var result = difference();
-  if (result !==0) throw new Error('Expected sum() to be 0. Received :' + result);
+  if (result !==0) throw new Error('Expected difference() to be 0. Received :' + result);
 
   console.log('All tests passed successfully.');
 
