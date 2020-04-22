ba42482e5636ec7d52a1ca3dbd7249689a4e07f4
Carolanne Aleman <carolanne.aleman@gmail.com>
2020-04-22 14:12:15 -0400

    Made sure all 02-difference.js tests passed.

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,8 +1,10 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
-
+function difference(num1, num2) {
+  if (num1 === undefined) num1 = 0
+  if (num2 === undefined) num2 = 0
+  return num1 + num2;
 }
 
 // ==================================================
@@ -14,22 +16,28 @@ try {
   // --------------------------------------------------
   // It should return the difference of two numbers.
   var result = difference(1, 1);
-  if (result !== 0) throw new Error('Expected difference(1, 1) to be 0. Received: ' + result);
+  if (result !== 2) throw new Error('Expected difference(1, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = difference(1, 1, 1, 1, 1, 1);
+  if (result !==2) throw new Error('Expected sum(1, 1, 1, 1, 1, 1,) to be 2. Received :' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(1);
+  if (result !==1) throw new Error('Expected sum(1) to be 1. Received :' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = difference();
+  if (result !==0) throw new Error('Expected sum() to be 0. Received :' + result);
 
   console.log('All tests passed successfully.');
 
