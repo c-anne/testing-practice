1f69706b1d7f97f6caee2be417befdee25cb63a9
Carolanne Aleman <carolanne.aleman@gmail.com>
2020-04-23 15:47:25 -0400

    Made sure all test passed for calc.js and made adjustments to include other files into calc.js

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,8 +1,28 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function calc() {
+const sum = require('../01-unit/01-sum.js').sum
+const difference = require('../01-unit/02-difference.js').difference
+const product = require('../01-unit/03-product.js').product
+const quotient = require('../01-unit/04-quotient.js').quotient
+function calc(option, num1, num2) {
+  if(option === 'add'){
+      return sum(num1, num2);
+  }
+  if(option === 'subtract'){
+    return difference(num1, num2);
+}
+  if(option === 'multiply'){
+    return product(num1, num2);
 
+  }
+  if(option === 'divide'){
+    return quotient(num1, num2);
+
+  }
+  else {
+    return 'Operation not supported';
+  }
 }
 
 // ==================================================
@@ -20,24 +40,27 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
+  var result = calc('subtract', 20, 10);
+  if (result !== 10) throw new Error('Expected calc("subtract", 20, 10) to be 10. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
-
+  var result = calc('multiply', 9, 9);
+  if (result !== 81) throw new Error('Expected calc("multiply", 9, 9) to be 81. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
-
+  var result = calc('divide', 9, 3);
+  if (result !== 3) throw new Error('Expected calc("multiply", 9, 3) to be 3. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
-
-  console.log('All tests passed successfully.');
-
+  var result = calc('exponent', 2, 8);
+  if (result !== 'Operation not supported') throw new Error('Expected exponent to be "Operation not supported". Received: ' + result);
 // ==================================================
 // PRINT ERRORS
 // ==================================================
